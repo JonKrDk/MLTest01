bd7ac8abe9b3799a6a523ee4b35514f52b1770d0
Jon Kristensen <jon@jonkr.dk>
2025-11-22 20:07:21 +0100

    Added DataViewHelper to visualize IDataView in debugger

---
+++ MLTest01.WebApp/DataViewHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ML;
+using System.Data;
+
+namespace MLTest01.WebApp
+{
+    public static class DataViewHelper
+    {
+        public static DataTable? ToDataTable(this IDataView dataView)
+        {
+            DataTable? dataTable = null;
+
+            if (dataView is not null)
+            {
+                dataTable = new DataTable();
+                var preview = dataView.Preview();
+                dataTable.Columns.AddRange(preview.Schema.Select(column => new DataColumn(column.Name, typeof(object))).ToArray());
+                foreach (var row  in preview.RowView)
+                {
+                    var dataRow = dataTable.NewRow();
+                    foreach (var column in row.Values)
+                    {
+                        dataRow[column.Key] = column.Value ?? DBNull.Value;
+                    }
+                    dataTable.Rows.Add(dataRow);
+                }
+            }
+
+            return dataTable;
+        }
+    }
+}
