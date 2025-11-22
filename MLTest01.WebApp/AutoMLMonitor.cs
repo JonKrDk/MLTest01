ec5d19e2006ddaa869648c06c430d08c9b9df484
Jon Kristensen <jon@jonkr.dk>
2025-11-22 16:41:10 +0100

    Add project files.

---
+++ MLTest01.WebApp/AutoMLMonitor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.ML.AutoML;
+
+namespace MLTest01.WebApp
+{
+    public class AutoMLMonitor : IMonitor
+    {
+        private readonly List<TrialResult> _completedTrials;
+        private readonly SweepablePipeline _pipeline;
+        private readonly ILogger<AutoMLMonitor> logger;
+
+        public AutoMLMonitor(SweepablePipeline pipeline, ILogger<AutoMLMonitor> logger)
+        {
+            _completedTrials = new List<TrialResult>();
+            _pipeline = pipeline;
+            this.logger = logger;
+        }
+
+        public IEnumerable<TrialResult> GetCompletedTrials() => _completedTrials;
+
+        public void ReportBestTrial(TrialResult result)
+        {
+            return;
+        }
+
+        public void ReportCompletedTrial(TrialResult result)
+        {
+            var trialId = result.TrialSettings.TrialId;
+            var timeToTrain = result.DurationInMilliseconds;
+            var pipeline = _pipeline.ToString(result.TrialSettings.Parameter);
+            logger.LogDebug($"Trial {trialId} finished training in {timeToTrain}ms with pipeline {pipeline}");
+            _completedTrials.Add(result);
+        }
+
+        public void ReportFailTrial(TrialSettings settings, Exception exception = null)
+        {
+            if (exception.Message.Contains("Operation was canceled."))
+            {
+                logger.LogDebug($"{settings.TrialId} cancelled. Time budget exceeded.");
+            }
+            logger.LogDebug($"{settings.TrialId} failed with exception {exception.Message}");
+        }
+
+        public void ReportRunningTrial(TrialSettings setting)
+        {
+            return;
+        }
+    }
+}
